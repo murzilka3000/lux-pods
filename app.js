f58b862ad35686fb6bd844155a56b562a1c4e922
Nikolay <nikolayzhidkov@icloud.com>
2025-03-10 19:43:43 +0700

    100

---
+++ app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const swiperCat = new Swiper('.swiper-cat', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 3, // Показывать по одному слайду
+      spaceBetween: 10, // Отступ между слайдами
+  
+      // Navigation arrows
+      navigation: {
+        nextEl: '.next',
+        prevEl: '.prev',
+      },
+    });
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const swiperCat = new Swiper('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 4, // Показывать по одному слайду
+      spaceBetween: 20, // Отступ между слайдами
+  
+      // Navigation arrows
+      navigation: {
+        nextEl: '.next-2',
+        prevEl: '.prev-2',
+      },
+    });
+  });
