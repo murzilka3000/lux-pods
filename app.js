0f96f579b8627456e24c7c6746280582742b3a20
Nikolay <nikolayzhidkov@icloud.com>
2025-03-12 17:27:06 +0700

    100

--- app.js
+++ app.js
@@ -15,21 +15,23 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const swiperCat = new Swiper('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      slidesPerView: 4, // Показывать по одному слайду
-      spaceBetween: 20, // Отступ между слайдами
-  
-      // Navigation arrows
-      navigation: {
-        nextEl: '.next-2',
-        prevEl: '.prev-2',
-      },
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.swiper-container').forEach((slider, index) => {
+        const prevButton = slider.parentElement.querySelector('.prev-2');
+        const nextButton = slider.parentElement.querySelector('.next-2');
+
+        new Swiper(slider, {
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 4,
+            spaceBetween: 20,
+            navigation: {
+                nextEl: nextButton,
+                prevEl: prevButton,
+            },
+        });
     });
-  });
+});
 
 
   document.addEventListener('DOMContentLoaded', function() {
@@ -47,3 +49,15 @@ document.addEventListener('DOMContentLoaded', function() {
       },
     });
   });
+
+
+
+
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
